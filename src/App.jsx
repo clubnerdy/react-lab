7001dacd986d9ab84ef08155151e3d5bbad53221
SeoHoejoeng <seohoejeong@gmail.com>
2026-03-30 17:18:28 +0900

    hero 섹션 ui 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./sections/Hero";
 import Nav from "@/components/Nav";
+import Skill from "./sections/Skill";
 
 function App() {
   return (
     <>
       <Hero />
       <Nav />
+      <div className="box"></div>
     </>
   );
 }
