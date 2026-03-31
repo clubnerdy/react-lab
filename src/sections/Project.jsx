61595ff81c5232fe7ee71e3fb35221b2a21b974f
SeoHoejoeng <seohoejeong@gmail.com>
2026-03-31 16:53:25 +0900

    project ui 진행중

---
+++ src/sections/Project.jsx
@@ -0,0 +1,15 @@
+import "./Project.css";
+
+const Project = () => {
+  return (
+    <div className="Project">
+      <div className="inner">
+        <div className="top">
+          <p className="description">프로젝트 상세보기</p>
+          <h3 className="title">프로젝트 세부 사항을 확인해 보세요</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Project;
