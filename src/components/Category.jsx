871edfaf47ab2315a0d1145c3a8c07cba86037c7
SeoHoejoeng <seohoejeong@gmail.com>
2026-03-31 17:04:57 +0900

    career 수정

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,12 +1,18 @@
 import "./Category.css";
 
-const Category = () => {
+const Category = ({ tabs, isActive, onTabChange }) => {
   return (
     <div className="Category">
       <ul>
-        <li className="active">웹 개발</li>
-        <li>브랜딩 / 디자인</li>
-        <li>콘텐츠 / 운영</li>
+        {tabs.map((tab) => (
+          <li
+            key={tab.id}
+            onClick={onTabChange}
+            className={isActive == tab.id ? "active" : ""}
+          >
+            {tab.label}
+          </li>
+        ))}
       </ul>
     </div>
   );
