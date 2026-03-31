5d10627feb5d05a093d9356f775a6f7204fc265d
SeoHoejoeng <seohoejeong@gmail.com>
2026-03-31 13:29:38 +0900

    ability ui 완료

--- src/sections/Ability.jsx
+++ src/sections/Ability.jsx
@@ -1,6 +1,19 @@
+import AbilityCard from "@/components/card/AbilityCard";
 import "./Ability.css";
+import { abilities } from "@/data/abilities";
 
 const Ability = () => {
-  return <div className="Ability"></div>;
+  return (
+    <div className="Ability">
+      <div className="inner">
+        <h3 className="page_title">역량</h3>
+        <div className="card_list">
+          {abilities.map((item) => (
+            <AbilityCard key={item.id} {...item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Ability;
