a480029975a50009c622404bff4d14675bafab94
Seo Hoejeong <139827011+clubnerdy@users.noreply.github.com>
2026-04-14 16:37:17 +0900

    [fix] Blog 기능개발 진행중에 따른 임시 내용 변경 (#34)
    - Blog 기능개발 진행중에 따른 임시 내용 변경

--- src/sections/Blog.tsx
+++ src/sections/Blog.tsx
@@ -10,42 +10,12 @@ const Blog = () => {
       <div className="inner">
         <h3 className="page_title">블로그</h3>
         <div className="blog_list">
-          <BlogCard
-            link={"/"}
-            imgUrl={examImage}
-            date={"2026.03.31"}
-            title={"[React] 45. New 페이지 기능 구현"}
-          />
-          <BlogCard
-            link={"/"}
-            imgUrl={examImage}
-            date={"2026.03.31"}
-            title={"[React] 45. New 페이지 기능 구현"}
-          />
-          <BlogCard
-            link={"/"}
-            imgUrl={examImage}
-            date={"2026.03.31"}
-            title={"[React] 45. New 페이지 기능 구현"}
-          />
-          <BlogCard
-            link={"/"}
-            imgUrl={examImage}
-            date={"2026.03.31"}
-            title={"[React] 45. New 페이지 기능 구현"}
-          />
-          <BlogCard
-            link={"/"}
-            imgUrl={examImage}
-            date={"2026.03.31"}
-            title={"[React] 45. New 페이지 기능 구현"}
-          />
-          <BlogCard
-            link={"/"}
-            imgUrl={examImage}
-            date={"2026.03.31"}
-            title={"[React] 45. New 페이지 기능 구현"}
-          />
+          <BlogCard link={"/"} imgUrl={examImage} date={""} title={"준비중"} />
+          <BlogCard link={"/"} imgUrl={examImage} date={""} title={"준비중"} />
+          <BlogCard link={"/"} imgUrl={examImage} date={""} title={"준비중"} />
+          <BlogCard link={"/"} imgUrl={examImage} date={""} title={"준비중"} />
+          <BlogCard link={"/"} imgUrl={examImage} date={""} title={"준비중"} />
+          <BlogCard link={"/"} imgUrl={examImage} date={""} title={"준비중"} />
         </div>
         <a href="https://inblog.ai/clubnerdy" target="_blank" rel="noreferrer">
           블로그로 이동 <img src={linkIcon} alt="" />
